582495a6215372bd683bc47a21d6b9703d935e3d
jaspervermeulen <jaspervermeulen.be@icloud.com>
2021-06-01 12:32:13 +0200

    Add reponsiveness

--- src/view/layout.php
+++ src/view/layout.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <!-- <link rel="stylesheet" href="./css/reset.css">
-    <link rel="stylesheet" href="./style.css"> -->
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./style.css">
 
     <?php echo $css;?>
 
@@ -16,26 +16,21 @@
 
 <body>
 
-    <header class="header">
-
-        <h1 class="hidden">Knot</h1>
-        <nav class="header__nav">
-            <ul class="nav__list">
-                <li class="list__item list__item--logo"><a class="list__link list__link--special"
-                        href="index.php?page=home">Knot</a></li>
-                <li></li>
-                <li class="list__item list__item--margintop "><a class="list__link <?php if($page == 'About') echo 'active'; ?>" href="index.php?page=about">Over ons</a></li>
-                <li class="list__item list__item--margintop"><a class="list__link <?php if($page == 'Agenda') echo 'active'; ?>" href="index.php?page=agenda">Agenda</a></li>
-                <li class="list__item list__item--margintop"><a class="list__link <?php if($page == 'Contact') echo 'active'; ?>" href="index.php?page=contact">Contact</a></li>
-                <li></li>
-                <li class="list__item list__item--margintop list__item--button"><a class="list__link list__link--button"
-                        href="index.php?page=member">Word lid!</a></li>
-                <li class="list__item list__item--margintop list__item--button"><a
-                        class="list__link list__link--buttonticket" href="index.php?page=tickets">Koop ticket</a></li>
-            </ul>
-        </nav>
-
-    </header>
+  <header class="header">
+    <h1 class="hidden">Knot</h1>
+    <nav class="header__nav">
+      <a class="header__nav--logo" href="index.php?page=home">Knot</a>
+      <div class="header__nav--links">
+        <a class="header__nav--links-item" <?php if($page == 'About') echo 'active'; ?>" href="index.php?page=about">Over ons</a>
+        <a class="header__nav--links-item" <?php if($page == 'Agenda') echo 'active'; ?>" href="index.php?page=agenda">Agenda</a>
+        <a class="header__nav--links-item" <?php if($page == 'Contact') echo 'active'; ?>" href="index.php?page=contact">Contact</a>
+      </div>
+      <div class="header__nav--buttons">
+        <a class="header__nav--buttons-button-1" href="index.php?page=member">Word lid!</a>
+        <a class="header__nav--buttons-button-2" href="index.php?page=tickets">Koop ticket</a>
+      </div>
+    </nav>
+  </header>
 
     <?php echo $content; ?>
 
@@ -51,8 +46,8 @@
 
     <?php echo $js; ?>
 
-    <!-- <script src="index.js"></script>
-    <script type="text/javascript" src="js/validate.js"></script> -->
+    <script src="index.js"></script>
+    <script type="text/javascript" src="js/validate.js"></script>
 </body>
 
 </html>
