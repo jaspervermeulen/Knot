c9f5214eb1c9f53eb8a4ad37132f61459cda204e
Jasper Vermeulen <56585012+jaspervermeulen@users.noreply.github.com>
2021-06-17 11:17:15 +0200

    Update layout.php

--- src/view/layout.php
+++ src/view/layout.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="./css/reset.css">
-    <link rel="stylesheet" href="./style.css">
+    
 
     <?php echo $css;?>
 
@@ -46,8 +45,7 @@
 
     <?php echo $js; ?>
 
-    <script src="index.js"></script>
-    <script type="text/javascript" src="js/validate.js"></script>
+    
 </body>
 
 </html>
